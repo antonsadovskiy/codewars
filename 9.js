c5a1ba8f5c456e67e136e86410f4a54d1dfe6e47
Anton Sadovskiy <antonsadovskiy6@gmail.com>
2022-12-04 13:30:52 +0300

    remove vowels from a string

--- 9.js
+++ 9.js
@@ -1,11 +1,3 @@
-function squareDigits(num){
-    let final = [];
-    let numsArr = Array.from(String(num), Number);
-    for (let i = 0; i < numsArr.length; i++){
-        let count = parseInt(numsArr[i]);
-        final.push += Math.pow(count)
-    }
-    return final;
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
   }
-
-console.log(squareDigits(938247));
