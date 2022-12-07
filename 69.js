caeed056a5badb163b3322a008fdc100877cc025
Anton Sadovskiy <antonsadovskiy6@gmail.com>
2022-12-07 11:26:20 +0300

    Sum Arrays

---
+++ 69.js
@@ -0,0 +1,10 @@
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+      sum += numbers[i]
+    }
+    return sum
+    
+};
