b4030f45bb57a5363cacaa40eb518b9cf3bc7415
Anton Sadovskiy <antonsadovskiy6@gmail.com>
2023-01-09 16:34:21 +0300

    Welcome to the City

---
+++ 150.js
@@ -0,0 +1,8 @@
+function sayHello( name, city, state ) {
+    let helloString = 'Hello, '
+    for (let i = 0; i < name.length - 1; i++){
+      helloString += name[i] + ' ';
+    }
+    helloString += name[name.length - 1]
+    return `${helloString}! Welcome to ${city}, ${state}!`
+  }
