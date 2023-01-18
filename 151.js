4f8a78bc14b7bfce56485253261667f09c990f31
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-18 22:31:59 +0300

    151.js

---
+++ 151.js
@@ -0,0 +1,14 @@
+function findLongestWordLength(str) {
+    let string = str.split(' ');
+
+    let maxWord = string[0]
+
+    for (let i = 0; i < string.length; i++){
+        if (string[i].length > maxWord.length){
+            maxWord = string[i]
+        }
+    }
+    return maxWord.length
+  }
+  
+console.log(findLongestWordLength("Google do a barrel roll"));
